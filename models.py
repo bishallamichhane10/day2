1f0ccf527c43277437ebc3c6cbb84d987d230e3f
bishal <lamichhanebishal556@gmail.com>
2022-07-16 15:53:37 +0545

    added api

--- models.py
+++ models.py
@@ -11,3 +11,13 @@ class student(base):
     first_name = Column('first name',NullType)
     last_name = Column ('last name',NullType)
     dob = Column('dob',NullType)
+
+class Person(base):
+    __tablename__='table_person'
+    id = Column(Integer,primary_key = True, index = True)
+    person_username = Column('username', NullType)
+    person_password = Column('password',NullType)
+    person_email = Column('email',NullType)
+    blog_id = Column('blog_id',NullType)
+
+
